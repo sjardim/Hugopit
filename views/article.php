1de8d7a11840ed97691a9e27a91c16d784d2ea91
Sérgio Alves Lima Jardim <sergio@plasmadesign.com.br>
2015-12-18 03:30:57 -0200

    Cleanup

--- views/article.php
+++
@@ -1,9 +0,0 @@
-<h1><?php echo $post['title'];?></h1>
-<small><?php echo date("c",$post["created"]); ?></small>
-<p><?php echo $post['categorias']; ?></p>
-<p>    
-    <?php 
-      // set $extra to true to render as MarkdownExtra
-      markdown($post['content'], $extra = true);
-    ?> 
-</p>
